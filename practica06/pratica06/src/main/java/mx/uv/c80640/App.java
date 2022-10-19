5a31ac712aff65ededfe5c3a3692ac3362c8ff7d
ic4rta <oxmars12@gmail.com>
2022-10-19 14:03:20 -0500

    practica06

---
+++ practica06/pratica06/src/main/java/mx/uv/c80640/App.java
@@ -0,0 +1,12 @@
+package mx.uv.c80640;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
